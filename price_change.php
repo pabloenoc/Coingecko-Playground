0d32154823bdba873bf00a54ed23ff9051fdd330
Pablo Enoc <pablo@enocc.com>
2025-09-14 11:10:21 -0700

    navbar

--- price_change.php
+++ price_change.php
@@ -1,13 +1,3 @@
-<?php
-
-if (isset($_GET["query"])) {
-    $url = "https://api.coingecko.com/api/v3/search?query=" . $_GET["query"];
-    $result = file_get_contents($url);
-    $data = file_put_contents("data/search-results.log", $result . "\n", FILE_APPEND);
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -66,17 +56,9 @@ if (isset($_GET["query"])) {
         </a>
     </nav>
 
-    <h1>Coingecko Search</h1>
+    <h1><a href="/" style="color:inherit; text-decoration: none;">Coingecko Price Change</a></h1>
 
-    <form action="search.php" method="GET">
-        <label for="search">Query</label>
-        <input type="search" name="query">
-        <input type="submit" value="Search">
-    </form>
 
-    <?php if (isset($result)) : ?>
-        <p><code><?= htmlspecialchars($result); ?></code></p>
-    <?php endif; ?>
 
 </body>
 
