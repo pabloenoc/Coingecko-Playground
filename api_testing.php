918bcfbb9122578ddf4b8a624b8e5f6b86b89654
Pablo Enoc <pablo@enocc.com>
2025-10-12 11:50:10 -0700

    view partials

---
+++ api_testing.php
@@ -0,0 +1,16 @@
+<?php
+// Temp file test
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php require __DIR__ . "/views/_head.php" ?>
+
+<body class="container">
+  <?php require __DIR__ . "/views/_nav.php" ?>
+
+  <h1><a href="/" style="color:inherit; text-decoration: none;">API Testing</a></h1>
+
+
+</body>
+</html>
--- price_change.php
+++ price_change.php
@@ -57,51 +57,7 @@ if (isset($coingecko_api_url)) {
 
 <body class="container">
 
-    <nav style="justify-content: flex-end; gap: 1rem; padding: 1rem;">
-
-        <a href="/">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="1"
-                stroke-linecap="round"
-                stroke-linejoin="round">
-                <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-                <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-            </svg>
-
-            Home
-        </a>
-
-        <a href="search.php">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-            </svg>
-            Search API
-        </a>
-
-        <a href="price_change.php">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="1"
-                stroke-linecap="round"
-                stroke-linejoin="round">
-                <path d="M4 19l16 0" />
-                <path d="M4 15l4 -6l4 2l4 -5l4 4" />
-            </svg>
-            Price Change
-        </a>
-    </nav>
+    <?php require __DIR__ . "/views/_nav.php" ?>
 
     <h1><a href="/" style="color:inherit; text-decoration: none;">Coingecko Price Change</a></h1>
 
