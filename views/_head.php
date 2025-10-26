8d446a5580ac3c34373625d452fe0ed5d9898c2e
Pablo Enoc <pablo@enocc.com>
2025-10-26 12:04:40 -0700

    include chartjs in head

--- views/_head.php
+++ views/_head.php
@@ -2,5 +2,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>Coingecko Playground</title>
+    <style>
+        :root {
+            --pico-font-size: 92%;
+        }
+    </style>
 </head>
