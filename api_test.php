e63ea3c35693a0e590ec925a9b842b46ae42c68a
Pablo Enoc <pablo@enocc.com>
2025-11-23 10:44:05 -0800

    fix missing chartjs dependency

--- api_test.php
+++ api_test.php
@@ -47,12 +47,7 @@ if (isset($coingecko_api_url)) {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
-    <title>API Test</title>
-</head>
+<?php require "views/_head.php" ?>
 
 <body class="container">
 
