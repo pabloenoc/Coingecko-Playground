a16e4c5d5b5c14e433f8bcf8925ba6f433b084ff
Pablo Enoc <pablo@enocc.com>
2025-11-23 12:15:35 -0800

    make year php based

--- views/_footer.php
+++ views/_footer.php
@@ -1,3 +1,3 @@
 <footer>
-	<p><small>© 2025 Pablo Enoc. All <a href="https://github.com/pabloenoc/Coingecko-Playground">code</a> is licensed under MIT.</small></p>
+	<p><small>© <?= date('Y') ?> Pablo Enoc. All <a href="https://github.com/pabloenoc/Coingecko-Playground">code</a> is licensed under MIT.</small></p>
 </footer>
